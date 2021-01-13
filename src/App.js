6031136766739bb34fe2a3ca552bbc76881abde0
Guillou33 <autiergilles@gmail.com>
2021-01-14 00:56:20 +0100

    firebase connected

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import logo from "./logo.svg";
+import React, { useState } from 'react';
+import './App.css';
+import {firebase, messaging} from './services/firebase'
+import { useEffect } from 'react';
 import "./App.css";
+const fb = firebase;
 
 function App() {
-  return <div className="App"></div>;
+  
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = fb.firestore().collection('Users').orderBy('age', 'desc').onSnapshot(u => {
+      setUsers(u.docs.map(user => {
+        return {id: user.id, ...user.data()}
+      }))
+    });
+
+    return () => unsubscribe()
+  }, []);
+
+  return <div className="App">
+    <h1>Stay Alive</h1>
+    <ul>
+      {users.map((u) => {
+        return (<li key={u.id}>{u.name} {u.age}</li>);
+      })}
+    </ul>
+
+  </div>;
 }
 
 export default App;
---
+++ src/services/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import "firebase/messaging"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBP5P7XpfVdmbn7lGVagGbbFzNC5C13i-k",
+  authDomain: "stayalive-58f5e.firebaseapp.com",
+  databaseURL: "https://stayalive-58f5e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "stayalive-58f5e",
+  storageBucket: "stayalive-58f5e.appspot.com",
+  messagingSenderId: "594990641454",
+  appId: "1:594990641454:web:667bf9241834e8564f0abf"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey(
+	// Project Settings => Cloud Messaging => Web Push certificates
+  "BOityaS8GQid8Ey4X0HS5IUm_TbVGTpquC8q_2OnGAKXob6fJxMOVIXdaHPfkt6IHKgD300sA5wPMObc4RLUxRo"
+);
+
+export {firebase, messaging}

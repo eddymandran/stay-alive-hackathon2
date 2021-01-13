04e8db68d2ea510595ed372333dd51ccf623da2b
eddymandran <eddy.mandran@gmail.com>
2021-01-14 00:59:48 +0100

    mise en place du context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Game from './components/Game';
+import LayoutContextProvider from './Contexts/layoutContext'
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
+      <LayoutContextProvider>
       <Router>
         <Switch>
           <Route path='/game' component={Game} />
         </Switch>
       </Router>
+      </LayoutContextProvider>
     </div>
   );
 }

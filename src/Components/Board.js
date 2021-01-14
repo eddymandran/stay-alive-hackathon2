1ded628c72eee4d96e68ae9eb66826ea0cd5b0e3
Pella0 <pellattiero.ju@gmail.com>
2021-01-14 17:41:10 +0100

    continuité sur score commit pour transfere eddy

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-restricted-globals */
-import React from "react";
-import "../Styles/Board.css";
+import React, { useState } from 'react';
+import '../Styles/Board.css';
 
 export default function Board() {
+  const [scorePlayer, setScorePlayer] = useState(0);
   document.body.onmousemove = function () {
-    document.getElementById("scope").style.marginLeft = event.x - 32 + "px";
-    document.getElementById("scope").style.marginTop = event.y - 32 + "px";
+    document.getElementById('scope').style.marginLeft = event.x - 32 + 'px';
+    document.getElementById('scope').style.marginTop = event.y - 32 + 'px';
   };
 
   const speed = 22;
@@ -15,69 +16,70 @@ export default function Board() {
   document.body.onkeydown = function () {
     const myKey = event.keyCode;
 
-    const target = document.getElementById("target");
+    const target = document.getElementById('target');
     if (myKey === 37) {
       leftSpace -= speed;
-      target.style.marginLeft = leftSpace + "px";
+      target.style.marginLeft = leftSpace + 'px';
     } else if (myKey === 38) {
       topSpace -= speed;
-      target.style.marginTop = topSpace + "px";
+      target.style.marginTop = topSpace + 'px';
     } else if (myKey === 39) {
       leftSpace += speed;
-      target.style.marginLeft = leftSpace + "px";
+      target.style.marginLeft = leftSpace + 'px';
     } else if (myKey === 40) {
       topSpace += speed;
-      target.style.marginTop = topSpace + "px";
+      target.style.marginTop = topSpace + 'px';
     }
   };
 
-  const bulletSound = document.createElement("AUDIO");
+  const bulletSound = document.createElement('AUDIO');
   bulletSound.src =
-    "http://www.freesound.org/people/RandomationPictures/sounds/138480/download/138480__randomationpictures__bullet-blood-3.wav";
+    'http://www.freesound.org/people/RandomationPictures/sounds/138480/download/138480__randomationpictures__bullet-blood-3.wav';
 
   function shoot() {
     bulletSound.play();
     let bulletPosX = event.x - 32;
     console.log(event.x);
     let bulletPosY = event.y - 32;
-    let targetPosX = document.getElementById("target").x;
+    let targetPosX = document.getElementById('target').x;
     let targetPosXEdge = targetPosX + 100;
-    let targetPosY = document.getElementById("target").y;
+    let targetPosY = document.getElementById('target').y;
     let targetPosYEdge = targetPosY + 100;
-
+    let newScore = scorePlayer;
     if (
       bulletPosX > targetPosX &&
       bulletPosX < targetPosXEdge &&
       bulletPosY > targetPosY &&
       bulletPosY < targetPosYEdge
     ) {
+      newScore++;
+      setScorePlayer(newScore);  
+      }
       
-      document.getElementById("ScoreCount").innerHTML =
-        parseInt(document.getElementById("ScoreCount").innerHTML) + 1;
     } else {
-      document.getElementById("ScoreCount").innerHTML =
-        parseInt(document.getElementById("ScoreCount").innerHTML) - 1;
+      newScore--;
+      setScorePlayer(newScore); 
     }
   }
 
-  return (
 
-      <div id="container">
-        <img
-          src="http://vignette3.wikia.nocookie.net/callofduty/images/7/7c/L96A1_Scope_BO.png/revision/20120119151427"
-          id="scope"
-          onClick={shoot}
-          alt="scope"
-        />
-        <img
-          src="http://www.clker.com/cliparts/v/m/O/4/Z/t/target-board-hi.png"
-          id="target"
-          alt="target"
-        />
-        <h1>Shooting Game</h1>
-        <p>
-          Score: <span id="ScoreCount">0</span>
-        </p>
-      </div>
+  return (
+    <div id='container'>
+      <img
+        src='http://vignette3.wikia.nocookie.net/callofduty/images/7/7c/L96A1_Scope_BO.png/revision/20120119151427'
+        id='scope'
+        onClick={shoot}
+        alt='scope'
+      />
+      <img
+        src='http://www.clker.com/cliparts/v/m/O/4/Z/t/target-board-hi.png'
+        id='target'
+        alt='target'
+      />
+      <h1>Shooting Game</h1>
+      <p>
+        Score: <span id='ScoreCount'>{scorePlayer}</span>
+      </p>
+    </div>
   );
 }

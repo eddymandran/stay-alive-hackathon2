c9bb0a1fef19a8f3f7f0361b38aa1db514b0e9ad
cleroy117 <ce.leroy69@gmail.com>
2021-01-14 02:04:38 +0100

    creating user

--- src/App.js
+++ src/App.js
@@ -1,33 +1,72 @@
-import React, { useState } from 'react';
-import './App.css';
-import {firebase, messaging} from './services/firebase'
-import { useEffect } from 'react';
+import React, { useState } from "react";
+import "./App.css";
+import { firebase, messaging } from "./services/firebase";
+import { useEffect } from "react";
 import "./App.css";
 const fb = firebase;
 
 function App() {
-  
   const [users, setUsers] = useState([]);
+  const [pseudo, setPseudo] = useState("");
+  const [picturePath, setPicturePath] = useState("");
+
+  // Add a new document in collection "cities"
+  const createUser = (event) => {
+    event.preventDefault();
+    fb.firestore()
+      .collection("Users")
+      .doc("LA")
+      .set({
+        name: pseudo,
+        picturePath: picturePath,
+      })
+      .then(function () {
+        console.log("Document successfully written!");
+      })
+      .catch(function (error) {
+        console.error("Error writing document: ", error);
+      });
+  };
 
   useEffect(() => {
-    const unsubscribe = fb.firestore().collection('Users').orderBy('age', 'desc').onSnapshot(u => {
-      setUsers(u.docs.map(user => {
-        return {id: user.id, ...user.data()}
-      }))
-    });
+    const modifyUser = fb
+      .firestore()
+      .collection("Users")
+      .orderBy("name", "asc")
+      .onSnapshot((u) => {
+        setUsers(
+          u.docs.map((user) => {
+            return { id: user.id, ...user.data() };
+          })
+        );
+      });
 
-    return () => unsubscribe()
+    return () => modifyUser();
   }, []);
 
-  return <div className="App">
-    <h1>Stay Alive</h1>
-    <ul>
-      {users.map((u) => {
-        return (<li key={u.id}>{u.name} {u.age}</li>);
-      })}
-    </ul>
-
-  </div>;
+  return (
+    <div className="App">
+      <h1>Stay Alive</h1>
+      <ul>
+        {users.map((u) => {
+          return (
+            <li key={u.id}>
+              {u.name} {u.age}
+            </li>
+          );
+        })}
+      </ul>
+      <form onSubmit={createUser}>
+        <input
+          type="text"
+          placeholder="pseudo"
+          value={pseudo}
+          onChange={(e) => setPseudo(e.target.value)}
+        ></input>
+        <input type="submit" value="Rejoindre la partie" />
+      </form>
+    </div>
+  );
 }
 
 export default App;

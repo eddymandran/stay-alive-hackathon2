4b5d02f3614a998dda3706e4e61414376869d5ef
cleroy117 <ce.leroy69@gmail.com>
2021-01-14 11:43:56 +0100

    modify document

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import React, { useState } from "react";
 import "./App.css";
 import { firebase, messaging } from "./services/firebase";
 import { useEffect } from "react";
-import upload from './uploadFiles/uploadFiles';
 import "./App.css";
+
 const fb = firebase;
 
 function App() {
   const [users, setUsers] = useState([]);
   const [pseudo, setPseudo] = useState("");
-  const [picture, setPicture] = useState(null);
 
   // Add a new document in collection "cities"
   const createUser = (event) => {
     event.preventDefault();
-    //upload(picture);
     fb.firestore()
       .collection("Users")
       .doc("1oZUtkBNEGob0MnumsgU")
       .set({
         name: pseudo,
-        picturePath: picture.name,
       })
       .then(function () {
         console.log("Document successfully written!");
@@ -65,7 +62,6 @@ function App() {
           value={pseudo}
           onChange={(e) => setPseudo(e.target.value)}
         ></input>
-        <input type='file' placeholder='Importer votre avatar' onChange={(e) => setPicture(e.target.files[0])}/>
         <input type="submit" value="Rejoindre la partie" />
       </form>
     </div>
--- src/uploadFiles/uploadFiles.js
+++ src/uploadFiles/uploadFiles.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "file-storage");
+    cb(null, "../file-storage");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);

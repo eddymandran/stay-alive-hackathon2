c6cc71be988a744792e1127a9d7086498697cca1
Pella0 <pellattiero.ju@gmail.com>
2021-01-15 00:44:05 +0100

    correction du jeux pour que le score fonctionne en +1 et -1

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,8 +3,8 @@ import React, { useContext } from 'react';
 import '../Styles/Board.css';
 import { GeneralContext } from '../Contexts/GeneralContext';
 import nerf from '../images/nerf.png';
-import schwarzy from "../images/schwarzy.png";
-import { firebase } from "../services/firebase";
+import schwarzy from '../images/schwarzy.png';
+import { firebase } from '../services/firebase';
 const fb = firebase;
 
 export default function Board(props) {
@@ -19,34 +19,35 @@ export default function Board(props) {
   let leftSpace = 0;
   let topSpace = 0;
 
-  const handleScorePlayer = async (scorePlayer, iduser) =>{
-    await fb.firestore()
-    .collection("Users")
-    .doc(iduser)
-    .update({
-      score: scorePlayer
-    })
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.error("Error writing coordonates", err);
-    });
-  }
+  const handleScorePlayer = async (scorePlayer, iduser) => {
+    await fb
+      .firestore()
+      .collection('Users')
+      .doc(iduser)
+      .update({
+        score: scorePlayer,
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error('Error writing coordonates', err);
+      });
+  };
 
   const setCoordonates = (e) => {
     fb.firestore()
-      .collection("Users")
+      .collection('Users')
       .doc(user.props.id)
       .update({
         x: e.clientX,
         y: e.clientY,
       })
       .then((res) => {
-        console.log("Coordonates updated");
+        console.log('Coordonates updated');
       })
       .catch((err) => {
-        console.error("Error writing coordonates", err);
+        console.error('Error writing coordonates', err);
       });
     /*     setUser({x: e.clientX, y: e.clientY});
     console.log(user); */
@@ -71,7 +72,6 @@ export default function Board(props) {
     }
   };
 
-
   function shoot() {
     let bulletPosX = event.x - 32;
     let bulletPosY = event.y - 32;
@@ -80,7 +80,7 @@ export default function Board(props) {
     let targetPosY = document.getElementById('target').y;
     let targetPosYEdge = targetPosY + 100;
     let newScore = scorePlayer;
-    
+
     if (
       bulletPosX > targetPosX &&
       bulletPosX < targetPosXEdge &&
@@ -89,44 +89,37 @@ export default function Board(props) {
     ) {
       newScore++;
       setScorePlayer(newScore);
-      
     } else {
       newScore--;
       setScorePlayer(newScore);
     }
     handleScorePlayer(newScore, user.props.id);
   }
-  
 
   return (
     <div id='container'>
-      
-      <div 
-        id='target'
+      <div
         style={{
-              position: "absolute",
-              top: `${user.props.y}px`,
-              left: `${user.props.x}px`,
-              zIndex: -1
-            }}>
-        <img
-            alt='target'
-            src={schwarzy}
-          />
+          position: 'absolute',
+          top: `${user.props.y}px`,
+          left: `${user.props.x}px`,
+          zIndex: -1,
+        }}
+      >
+        <img id='target' alt='target' src={schwarzy} />
         <h2>{user.props.name}</h2>
       </div>
-      
-        
+
       <img
         src={nerf}
         id='scope'
         onClick={shoot}
         alt='scope'
         style={{
-          position: "absolute"
+          position: 'absolute',
         }}
       />
-     
+
       <h1>STAY ALIVE !!</h1>
     </div>
   );

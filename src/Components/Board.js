7d5bd649eef3d667e5233d02078b2744f493ae27
eddymandran <eddy.mandran@gmail.com>
2021-01-14 14:06:24 +0100

    fix some stuff

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -6,6 +6,14 @@ import "../Styles/Board.css"
 
 export default function Board() {
 
+  const dotsContainer = document.getElementById("dots-container");
+  console.log("console log"+ dotsContainer)
+  let element = document.createElement("div");
+  console.log(element)
+  element.style.position = "absolute";
+  element.style.zIndex = 1000;
+  element.style.cursor = "move";
+
   
 
   return (

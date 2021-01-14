1f58971956a67f256512873e703447a17920b0b6
gclement28 <clementgarcin6@gmail.com>
2021-01-14 12:09:20 +0100

    LE CAROUSEL MARCHE

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Components/Home';
-import ChooseMap from './Components/ChooseMap';
-import Game from './Components/Game';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Components/Home";
+import ChooseMap from "./Components/ChooseMap";
+import Game from "./Components/Game";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
         <main>
           <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/ChooseMap' component={ChooseMap} />
-            <Route path='/Game' component={Game} />
+            <Route path="/" exact component={Home} />
+            <Route path="/choosemap" component={ChooseMap} />
+            <Route path="/name" component={Game} />
           </Switch>
         </main>
       </Router>
--- src/Components/ChooseMap.js
+++ src/Components/ChooseMap.js
@@ -1,5 +1,47 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import Carousel from "react-elastic-carousel";
+import Item from "./Item";
+import "../Styles/ChooseMap.scss";
+import Bureau1 from "../images/Bureau1.png";
+import Bibliotheque from "../images/Bibliotheque.png";
+import Foret from "../images/Foret.png";
+import Jardin from "../images/Jardin.png";
 
-export default function ChooseMap() {
-  return <div>Choose Map</div>;
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 },
+];
+
+function ChooseMap() {
+  return (
+    <div className="choosemap-body">
+      <h1 style={{ textAlign: "center" }}>Choose your map</h1>
+      <div className="App">
+        <Carousel breakPoints={breakPoints}>
+          <Item>
+            <img src={Bureau1} />
+          </Item>
+          <Item>
+            {" "}
+            <img src={Bibliotheque} />
+          </Item>
+          <Item>
+            <img src={Foret} />
+          </Item>
+          <Item>
+            <img src={Jardin} />
+          </Item>
+          <Item>Five</Item>
+          <Item>Six</Item>
+          <Item>Seven</Item>
+          <Item>Eight</Item>
+        </Carousel>
+      </div>
+    </div>
+  );
 }
+
+export default ChooseMap;

a097e96b22bb9007fc67662230df607de36ca7ee
cleroy117 <ce.leroy69@gmail.com>
2021-01-14 12:55:19 +0100

    cleaning app.js before merge

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
-import { firebase, messaging } from "./services/firebase";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home";
 import ChooseMap from "./Components/ChooseMap";
 import Game from "./Components/Game";
 import "./App.css";
 
-const fb = firebase;
-
 function App() {
   const [users, setUsers] = useState([]);
   const [pseudo, setPseudo] = useState("");
-
-  // Add a new document in collection "cities"
 
   return (
     <div className="App">

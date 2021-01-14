891ec70612d418d8db5a9399b97b07cc80e499b8
Pella0 <pellattiero.ju@gmail.com>
2021-01-14 12:14:16 +0100

    maj avec branch map1

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
 import '../Styles/Game.css';
+import Board from './Board';
 
 export default function Game() {
   return (
     <div className='gameBody'>
       <Navbar />
+      <Board />
     </div>
   );
 }

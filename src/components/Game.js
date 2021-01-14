854eca55a554b8a80ee8911d32350049445fa8f5
eddymandran <eddy.mandran@gmail.com>
2021-01-14 03:29:02 +0100

    creation d'une grille avec grid

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import PlayerBoard from './components/PlayerBoard';
-import BattleBoard from './components/BattleBoard';
-import Score
+import PlayerBoard from './PlayerBoard';
+import BattleBoard from './BattleBoard';
+import Score from'./Score';
 
 function Game() {
   return (
     <div className='Game'>
       <PlayerBoard />
       <BattleBoard />
+      <Score/>
     </div>
   );
 }
--- src/components/PlayerBoard.js
+++ src/components/PlayerBoard.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import { SQUARE_STATE, generateEmptyLayout } from './layoutHelpers';
+import '../styles/BoardPlayer.css'
 
-function PlayerBord() {
-  return <div className='PlayerBord'></div>;
+const  PlayerBoard = () => {
+
+  return <div className='PlayerBoard'>
+    <div className="divBoard" id="1"></div>
+    <div className="divBoard" id="2"></div>
+    <div className="divBoard" id="3"></div>
+    <div className="divBoard" id="4"></div>
+    <div className="divBoard" id="5"></div>
+    <div className="divBoard" id="6"></div>
+    <div className="divBoard" id="7"></div>
+    <div className="divBoard" id="8"></div>
+    <div className="divBoard" id="9"></div>
+    <div className="divBoard" id="10"></div>
+    <div className="divBoard" id="11"></div>
+    <div className="divBoard" id="12"></div>
+    <div className="divBoard" id="13"></div>
+    <div className="divBoard" id="14"></div>
+    <div className="divBoard" id="15"></div>
+    <div className="divBoard" id="16"></div>
+    <div className="divBoard" id="17"></div>
+    <div className="divBoard" id="18"></div>
+    <div className="divBoard" id="19"></div>
+    <div className="divBoard" id="20"></div>
+    <div className="divBoard" id="21"></div>
+    <div className="divBoard" id="22"></div>
+    <div className="divBoard" id="23"></div>
+    <div className="divBoard" id="24"></div>
+    <div className="divBoard" id="25"></div>
+  </div>;
 }
 
-export default PlayerBord;
+export default PlayerBoard;

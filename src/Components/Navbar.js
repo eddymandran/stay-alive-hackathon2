15eb3330a2477e3bef7c62dd1bf04f8c02655347
Pella0 <pellattiero.ju@gmail.com>
2021-01-15 12:52:17 +0100

    ajout audio win et loose

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,27 @@
-import React, { useContext, useEffect } from "react";
-import "../Styles/Navbar.css";
-import { Howl } from "howler";
-import gagne from "../audio/gagne.mp3";
-import pump from "../audio/pump.mp3";
-import { GeneralContext } from "../Contexts/GeneralContext";
-import { useToasts } from "react-toast-notifications";
-import { firebase } from "../services/firebase";
-import { useState } from "react";
-import gun from "../images/gun.png";
-import schwarzy from "../images/schwarzy.png";
-import obama from "../images/obama.png";
-import madonna from "../images/madonna.png";
-import einstein from "../images/einstein.png";
-import lee from "../images/lee.png";
+import React, { useContext, useEffect } from 'react';
+import '../Styles/Navbar.css';
+import { Howl } from 'howler';
+import gagne from '../audio/gagne.mp3';
+import pump from '../audio/pump.mp3';
+import { GeneralContext } from '../Contexts/GeneralContext';
+import { useToasts } from 'react-toast-notifications';
+import { firebase } from '../services/firebase';
+import { useState } from 'react';
+import gun from '../images/gun.png';
+import schwarzy from '../images/schwarzy.png';
+import obama from '../images/obama.png';
+import madonna from '../images/madonna.png';
+import einstein from '../images/einstein.png';
+import lee from '../images/lee.png';
 
 const fb = firebase;
 
-// const audioClips = new Howl({
-//   src: [gagne],
-// });
-// const audioClips1 = new Howl({
-//   src: [pump],
-// });
+const audioClips = new Howl({
+  src: [gagne],
+});
+const audioClips1 = new Howl({
+  src: [pump],
+});
 
 export default function Navbar(props) {
   const { scorePlayer } = useContext(GeneralContext);
@@ -29,15 +29,15 @@ export default function Navbar(props) {
 
   function goodPlayer() {
     if (scorePlayer === 10 || scorePlayer === 20) {
-      //audioClips.play();
-      addToast("Vous êtes déchainé !", {
-        appearance: "success",
+      audioClips.play();
+      addToast('Vous êtes déchainé !', {
+        appearance: 'success',
         autoDismiss: true,
       });
     } else if (scorePlayer === -1 || scorePlayer === -10) {
-      //audioClips1.play();
-      addToast("Vous dormez ??? Debout la dedans !!!", {
-        appearance: "error",
+      audioClips1.play();
+      addToast('Vous dormez ??? Debout la dedans !!!', {
+        appearance: 'error',
         autoDismiss: true,
       });
     }
@@ -48,15 +48,15 @@ export default function Navbar(props) {
 
   const onChangePicture = (picture) => {
     switch (picture) {
-      case "schwarzy":
+      case 'schwarzy':
         return schwarzy;
-      case "obama":
+      case 'obama':
         return obama;
-      case "einstein":
+      case 'einstein':
         return einstein;
-      case "madonna":
+      case 'madonna':
         return madonna;
-      case "lee":
+      case 'lee':
         return lee;
       default:
         return schwarzy;
@@ -66,8 +66,8 @@ export default function Navbar(props) {
   useEffect(() => {
     const getUsers = fb
       .firestore()
-      .collection("Users")
-      .orderBy("name", "asc")
+      .collection('Users')
+      .orderBy('name', 'asc')
       .onSnapshot((u) => {
         setUsers(
           u.docs.map((user) => {
@@ -84,28 +84,28 @@ export default function Navbar(props) {
   }, [scorePlayer]);
 
   return (
-    <div className="scoreBar">
-      <div className="zoneAvatar">
-        <button className="buttonBack">Home</button>
+    <div className='scoreBar'>
+      <div className='zoneAvatar'>
+        <button className='buttonBack'>Home</button>
         <img
           src={onChangePicture(player.props.picture)}
-          className="avatarImage"
-          alt="personnage choisi"
+          className='avatarImage'
+          alt='personnage choisi'
         ></img>
-        <h5 className="title5">NickName : {player.props.name}</h5>
+        <h5 className='title5'>NickName : {player.props.name}</h5>
       </div>
       <hr />
-      <div className="zoneScore">
-        <h5 className="title5">
-          {" "}
-          Your score :<p className="scorePlayer">{scorePlayer}</p>
+      <div className='zoneScore'>
+        <h5 className='title5'>
+          {' '}
+          Your score :<p className='scorePlayer'>{scorePlayer}</p>
         </h5>
-        <img src={gun} alt="gun" className="gunImage"></img>
+        <img src={gun} alt='gun' className='gunImage'></img>
       </div>
       <hr />
-      <div className="zoneCharacters">
-        <h5 className="title5">Fugitive(s)</h5>
-        <ul className="charactersList">
+      <div className='zoneCharacters'>
+        <h5 className='title5'>Fugitive(s)</h5>
+        <ul className='charactersList'>
           {users.map((u) => {
             return (
               <li key={u.id}>
